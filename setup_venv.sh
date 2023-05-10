30ae934af8053eb3f7ffd2c7594404ee033a6a5f
Eduard Deaconu <eduard@mindliteracy.com>
2023-05-10 17:11:52 +0300

    updated setup_venv.sh script, removed unused requirements.txt

--- setup_venv.sh
+++ setup_venv.sh
@@ -1,4 +1,10 @@
+echo "The old ./venv/ folder will be removed"
+rm -r venv # remove possible older venv
 python3.10 -m venv venv
-. venv/bin/activate
-pip3.10 install -r requirements.txt
+source venv/bin/activate
+pip3.10 install numpy \
+                scipy \
+                matplotlib \
+                redis \
+                celery
 pip3.10 install git+https://github.com/pvigier/perlin-numpy
