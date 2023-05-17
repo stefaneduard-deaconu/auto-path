ea3a7d922f13b23018b21a8e0d00420f92e03fda
Eduard Deaconu <eduard@mindliteracy.com>
2023-05-17 16:51:37 +0300

    updated test to include HA and HAG assertion

--- test.py
+++ test.py
@@ -5,6 +5,13 @@ from tasks import task_terrain_from_config, task_HA_from_config, task_HAG_from_c
 # TODO Ed, height doesn't seem right
 # TODO Ed, save the data to a database
 res = task_terrain_from_config(config)
-print(res)
 res2 = task_terrain_from_config(config)
 assert res2 == res
+
+ha1 = task_HA_from_config(config)
+ha2 = task_HA_from_config(config)
+assert ha1 == ha2
+
+hag1 = task_HAG_from_config(config)
+hag2 = task_HAG_from_config(config)
+assert hag1 == hag2
